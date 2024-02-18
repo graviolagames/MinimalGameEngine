8945a2a1e08ea974bf78ea4c3987bd1a877cf275
graviolagames <graviolagames@gmail.com>
2024-02-18 14:41:37 -0300

    Create Cmake project and project Manager in python

---
+++ project_manager.py
@@ -0,0 +1,75 @@
+from project_manager_modules import process
+
+_deps_src_dir = '../deps_src' #directory for clonning source code of dependencies
+_deps_build_dir = '../deps_build' #directory for building dependencies
+_proj_build_dir = '../proj_build' #directory for building projects
+_from_proj_build_to_proj_src = "../../MinimalGameEngine" #path to go prom project build directory to project source directory
+_from_deps_builds_deps_src = "../../deps_src"
+
+# Method for setting up GLFW as dependency
+def glfw_setup():
+    
+    _glfw_repo_address = 'https://github.com/glfw/glfw.git'
+    _glfw_src_subdir = '/glfw'
+    _glfw_build_subdir = '/glfw'
+
+    #create a directory for source of dependencies
+    process.create_sub_dir(_deps_src_dir)
+    #create a directory for building dependencies
+    process.create_sub_dir(_deps_build_dir)
+    
+    # Clone source code for dependencies
+    process.clone_github_repo(_deps_src_dir,_glfw_repo_address)
+    
+    # Configure dependency projects
+    process.cmake_configure(_deps_build_dir + _glfw_build_subdir, _from_deps_builds_deps_src + _glfw_src_subdir)
+
+    # Build dependencies
+    process.cmake_build(_deps_build_dir + _glfw_build_subdir,'Release')
+    process.cmake_build(_deps_build_dir + _glfw_build_subdir,'Debug')
+
+# Set up all dependencies
+def dependencies_setup():
+    glfw_setup()
+
+# Build a project, guiven its build path and source path
+def build_project(build_path,src_path):
+    
+    #Create sub directory for building
+    process.create_sub_dir(build_path)
+    
+    #configure project
+    process.cmake_configure(build_path,src_path)
+    
+    #build project
+    process.cmake_build(build_path,'Debug')
+    process.cmake_build(build_path,'Release')
+
+# Set up a project ghiven its directory
+# 
+def setup_project(dir):
+    print("Building "+ dir)
+    build_project(_proj_build_dir + dir, _from_proj_build_to_proj_src + dir)
+
+def main():
+    while True:
+        print("\nChoose an action:")
+        print("d) (First) Download and build dependencies.")
+        print("........Configure and build........")
+        print("1) Minimal Game Engine.")
+        print("q) quit")
+        
+        choice = input("Choose an option: ")
+
+        if choice == "d":
+            dependencies_setup()
+        elif choice == "1":
+            setup_project("/MinimalGameEngine")
+        elif choice == "q":
+            print("Bye!")
+            break
+        else:
+            print("Invalid option, try again.")
+
+if __name__ == "__main__":
+    main()
---
+++ project_manager_modules/process.py
@@ -0,0 +1,27 @@
+import subprocess
+import os
+
+#Run a system command on a specific directory
+#[NOTE] Works only with OSes that use 'cd' to change directory
+
+def _run_command_in_directory(command= "", directory = ""):
+    full_command = f"cd {directory} && {command}"
+    print(full_command)
+    subprocess.run(full_command, shell=True)
+
+#Create a directory
+def create_sub_dir(dir = ""):
+    os.makedirs(dir, exist_ok=True)
+    
+#Clone a github repository 'repo' on specific 'directory' 
+def clone_github_repo(directory= "",repo = ""):
+    _run_command_in_directory("git clone " + repo,directory)
+
+#Configure project using cmake
+def cmake_configure(build_dir = "",src_dir = ""):
+    create_sub_dir(build_dir)
+    _run_command_in_directory("cmake " + src_dir,build_dir) 
+
+#Build project using cmake
+def cmake_build(build_dir,configuration):
+    _run_command_in_directory("cmake --build . --config " + configuration,build_dir)
